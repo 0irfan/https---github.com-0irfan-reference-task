ea337c745c43cb0d9e3c30f3de8945e8e77d765f
0irfan <irfanali66693@gmail.com>
2024-05-03 06:46:08 -0700

    changes

--- Reference/api/urls.py
+++ Reference/api/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import FileUpload,FileDownload
+from .views import Files,File,TaskList,TaskDetail
 
 urlpatterns = [
     
-    path('file/', FileUpload.as_view()),
-    path('file/<init:pk>',FileDownload.as_view(),)
+    path('reference-file/', Files.as_view()),
+    path('reference-file/<init:pk>',File.as_view()),
+    path('task-list',TaskList.as_view()),
+    path('task-detail',TaskDetail.as_view()),
 ]
